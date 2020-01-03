622e2a1cfb46ff80efe00df39a44f856b7f081cb
unknown <huutien280198@gmail.com>
2020-01-03 12:31:52 +0700

    Update model product

--- models/products.go
+++ models/products.go
@@ -15,6 +15,11 @@ type Product struct {
 	Status       string             `json:"status" bson:"status"`
 	ImageURL     string             `json:"imageUrl" bson:"image_url"`
 	ImageName    string             `json:"imageName" bson:"image_name"`
+	Size         []string           `json:"size" bson:"size"`
+	ColorName    []string           `json:"colorName" bson:"color_name"`
+	ColorCode    []string           `json:"colorCode" bson:"color_code"`
+	Material     string             `json:"material" bson:"material"`
+	Rate         int32              `json:"rate" bson:"rate"`
 	CategoryCode string             `json:"categoryCode" bson:"category_code"`
 	CategoryName string             `json:"categoryName" bson:"category_name"`
 	SelledAmount int32              `json:"selledAmount" bson:"selled_amount"`
